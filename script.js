e27f9b771fc98969f70166704e7d9bfacdf8345e
Vivan Dave <123893956+vivan64@users.noreply.github.com>
2025-03-12 18:02:39 -0400

    Created func displayDestinations

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(error => console.error("Error loading destinations:", error));
 });
+
+function displayDestinations(destination) {
+    const container = document.getElementById("destinations-container");
+    container.innerHTML = ""; //Clears previous results
+
+    destination.forEach(destination => {
+        const card = document.createElement("div");
+        card.classList.add("destination-card");
+
+        card.innerHTML = `
+            <img src="${destination.image}" alt="${destination.name}">
+            <h2>${destination.name}</h2>
+            <p>${destination.description}</p>
+            <button onclick="goToDetails(${destination.id})">View Details</button>
+            `;
+    })
+}
